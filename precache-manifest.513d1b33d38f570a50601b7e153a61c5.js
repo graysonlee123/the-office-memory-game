3e403b579c9cc8f89edb31564d32925363513538
Grayson Gantek <graysonleegantek@gmail.com>
2019-09-21 10:48:19 -0400

    Updates

---
+++ precache-manifest.513d1b33d38f570a50601b7e153a61c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c58e004d254416262f5e909c801d1a",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "96f28dc6133f22dc9661",
+    "url": "/memory-game/static/css/main.3d47e1b3.chunk.css"
+  },
+  {
+    "revision": "15eef4f98664e8847adc",
+    "url": "/memory-game/static/js/2.4a8f0937.chunk.js"
+  },
+  {
+    "revision": "96f28dc6133f22dc9661",
+    "url": "/memory-game/static/js/main.161a5d9d.chunk.js"
+  },
+  {
+    "revision": "a965dad33cfc69e1449b",
+    "url": "/memory-game/static/js/runtime~main.f417e672.js"
+  }
+]);
